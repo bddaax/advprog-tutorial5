c85450a368bb663abeda3294f2a3163622fbe9d7
bddaax <brenda147052gmail.com>
2025-03-14 19:50:30 +0700

    [Refactoring] - Optimizing method joinStudentNames

--- src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
+++ src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
@@ -35,13 +35,20 @@ public class StudentService {
                 .max(Comparator.comparingDouble(Student::getGpa));
     }
 
-
     public String joinStudentNames() {
         List<Student> students = studentRepository.findAll();
-        String result = "";
-        for (Student student : students) {
-            result += student.getName() + ", ";
+        if (students.isEmpty()) {
+            return "";
         }
-        return result.substring(0, result.length() - 2);
+
+        StringBuilder result = new StringBuilder();
+        for (Student student : students) {
+            result.append(student.getName()).append(", ");
+        }
+        
+        result.setLength(result.length() - 2);
+
+        return result.toString();
     }
+
 }
